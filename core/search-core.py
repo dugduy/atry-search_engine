d6d62d3d976bc9f361a612374e6ae24aec08173c
dugduy <dugduy01@gmail.com>
2022-06-04 08:19:43 +0700

    prepare to get data

---
+++ core/search-core.py
@@ -0,0 +1,12 @@
+knowlist=open('knew.txt',encoding='utf8').read().splitlines()
+def mysearch(kw):
+    result=[]
+    for i in knowlist:
+        if kw.lower().strip() in i.lower():
+            result.append(i)
+    return result
+kw=input('Search: ')
+result=mysearch(kw)
+print(len(result),'for',kw)
+for i in result:
+    print(i)
--- getinfofromasite.py
+++ getinfofromasite.py
@@ -1,12 +1,20 @@
 from requests import get
 from bs4 import BeautifulSoup
+from json import dump
 site='youtube.com'
-res=get('http://'+site)
-print(res)
-soup=BeautifulSoup(res.text,'lxml')
-# print(soup.prettify())
-print(soup.title.string)
-decryption=soup.find_all('meta',{'name':'description'})
-if len(decryption):
-    print(decryption[0]['content'])
-# print(soup.find_all('meta',{'name':'keywords'})[0]['content'])
+def getinfofrom(site):
+    res=get('http://'+site)
+    print(res)
+    soup=BeautifulSoup(res.text,'lxml')
+    # print(soup.prettify())
+    data={'name':site}
+    print(soup.title.string)
+    decryption=soup.find_all('meta',{'name':'description'})
+    if len(decryption):
+        print(decryption[0]['content'])
+        data['description']=decryption[0]['content']
+    kws=soup.find_all('meta',{'name':'keywords'})
+    if len(kws):
+        print(kws[0]['content'])
+        data['keywords']=kws[0]['content']
+    dump(data,open('./knw_sites/'+site,'w',encoding='utf8'))
